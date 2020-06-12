f29755f9463365e90e440812d8bd41a43aab5899
Roberto Portoni <roberto.portoni@caditgroup.com>
2020-06-12 16:57:48 +0200

    retromcapibility with JEE6: replaced ManagedScheduledExecutorService with TimerService

--- wildfly-mdb/src/main/java/com/cadit/cache/CacheManagerBean.java
+++ wildfly-mdb/src/main/java/com/cadit/cache/CacheManagerBean.java
@@ -3,13 +3,10 @@ package com.cadit.cache;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-import javax.enterprise.concurrent.ManagedScheduledExecutorService;
+import javax.ejb.*;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 
 @Singleton
@@ -17,11 +14,8 @@ import java.util.concurrent.TimeUnit;
 public class CacheManagerBean {
 
 
-    /**
-     * NB: su was 8.5 con JEE6 va sostituito con un @Timer schedulato
-     */
-    @Resource// usa il default di WildFly: lookup="java:jboss/ee/concurrency/scheduler/default"
-    private ManagedScheduledExecutorService executorService;
+    @Resource
+    private TimerService timerService;
 
     private ScheduledFuture<?> taskFuture;
 
@@ -30,12 +24,26 @@ public class CacheManagerBean {
 
 
     @PostConstruct
+    public void init(){
+        //schedule a task periodically each 8 secs
+        ScheduleExpression schedulerExpression = new ScheduleExpression()
+                .hour("*")
+                .minute("*")
+                .second("*/8"); // ogni 8 secondi
+        timerService.createCalendarTimer(schedulerExpression, new TimerConfig("CacheManagerTaskTimer",false));
+    }
+
+    @Timeout
+    @Lock(value = LockType.WRITE)
     public void runExpiredCheckerThread() {
         CacheManager cacheManager = CacheManager.getInstance();
         SoftCache.ExpireTimeAccessChecker task = cacheManager.expireTaskChecker();
-        //schedule a task periodically each 8 secs
-        taskFuture = executorService.scheduleAtFixedRate(task, 0, 8, TimeUnit.SECONDS);
+        task.run();
+
     }
+
+
+
 
     public String get(String key) {
         return CacheManager.getInstance().get(key);
